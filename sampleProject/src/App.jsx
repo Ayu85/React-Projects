d78f3fc2b1db89bf502f5b965a92520a0ed72a06
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-16 13:29:33 +0530

    react learning

--- sampleProject/src/App.jsx
+++ sampleProject/src/App.jsx
@@ -2,13 +2,26 @@ import { useState } from "react";
 import "./App.css";
 import Card1 from "./components/card1";
 function App() {
+  const items = [
+    {
+      name: "React",
+      year: "2023",
+      date: "15",
+      month: "September",
+    },
+  ];
   return (
     <>
-      <div className="bg-yellow-700 w-96 h-max rounded-xl flex flex-col gap-4 justify-center items-center py-5  ml-[50%] -translate-x-[50%] mt-20">
-        <Card1 name='React' year='2023' date='15' month='September'></Card1>
-        <Card1 name='Angular' year='2024' date='25' month='October'></Card1>
-        <Card1 name='Vue' year='2025' date='10' month='November'></Card1>
-        <Card1 name='Vite' year='2026' date='11' month='December'></Card1>
+      <div className="bg-yellow-700 w-[60vh] h-max rounded-xl flex flex-col gap-4 justify-center items-center py-5  ml-[50%] -translate-x-[50%] mt-20">
+        <Card1
+          name={items[0].name}
+          year="2023"
+          date="15"
+          month="September"
+        ></Card1>
+        <Card1 name="Angular" year="2024" date="25" month="October"></Card1>
+        <Card1 name="Vue" year="2025" date="10" month="November"></Card1>
+        <Card1 name="Vite" year="2026" date="11" month="December"></Card1>
       </div>
     </>
   );
--- sampleProject/src/components/card1.jsx
+++ sampleProject/src/components/card1.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { useState } from "react";
 function Card1({ name, date, month, year }) {
+  let [item, setItem] = useState({ name });
+  const itemAdded = function itemAdd() {
+    setItem("{Vercel}");
+    console.log({ name }, "added to cart");
+  };
+
   return (
-    <div className="flex justify-between py-3 pl-3 pr-3 items-center bg-slate-400 w-80 rounded-xl h-24">
-      <div className="bg-black h-20 text-white inline-flex flex-col justify-center items-center py-3 pl-3 pr-3 rounded-2xl">
+    <div className="flex justify-between py-3 pl-3 pr-3 items-center bg-slate-400 w-96 rounded-xl h-24">
+      <div className="bg-black h-20 text-white inline-flex flex-col justify-center items-center py-3 pl-3 pr-3 rounded-2xl w-32">
         <span>{month}</span>
         <span>{year}</span>
         <span>{date}</span>
       </div>
       <div className="text-2xl font-semibold text-white">{name}</div>
+      <button className="bg-white py-1 pl-1 pr-1 text-sm" onClick={itemAdded}>
+        View More
+      </button>
     </div>
   );
 }
